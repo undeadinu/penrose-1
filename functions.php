72a4b8b993006415b00389607452014816de1778
Michael Mckeever <mckeever02@gmail.com>
2016-04-10 21:55:27 +0100

    Remove scene.jpg

--- functions.php
+++ functions.php
@@ -258,7 +258,7 @@ function modify_jquery() {
 	if (!is_admin()) {
 		// comment out the next two lines to load the local copy of jQuery
 		wp_deregister_script('jquery');
-		wp_register_script('jquery', 'http://ajax.googleapis.com/ajax/libs/jquery/1.8.1/jquery.min.js', true, '1.8.1');
+		wp_register_script('jquery', 'http://ajax.googleapis.com/ajax/libs/jquery/1.8.1/jquery.min.js', false, '1.8.1');
 		wp_enqueue_script('jquery');
 	}
 }
