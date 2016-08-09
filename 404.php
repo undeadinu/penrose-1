c17ec99f12d10168ae005cb1121969c0d1418078
Michael Mckeever <mckeever02@gmail.com>
2016-08-09 11:54:22 +0100

    Switch to a more robust CSS grid

--- 404.php
+++ 404.php
@@ -3,7 +3,7 @@
 <main id="content">
 	<div class="wrapper">
 		<h1 class="title u-pull-left u-mb-0"><strong>404</strong> - Page not found</h1>
-		<p>The article you were looking for was not found. Try looking again.</p>
+		<p>The page you were looking for was not found. Try looking again.</p>
 
 	</div><!-- /container -->
 </main><!-- /#content -->
--- page.php
+++ page.php
@@ -1,30 +1,9 @@
 <?php get_header(); ?>
 
-	<main id="content">
+	<main class="wrapper">
 
-		<?php if (have_posts()) : while (have_posts()) : the_post(); ?>
 
 			<h1><?php the_title(); ?></h1>
-
-		<?php endif ?>
-
-
-
-			<?php endwhile; else : ?>
-
-					<article id="post-not-found" class="hentry">
-							<header class="article-header">
-								<h1><?php _e( 'Oops, Post Not Found!', 'bonestheme' ); ?></h1>
-						</header>
-							<section class="entry-content">
-								<p><?php _e( 'Uh Oh. Something is missing. Try double checking things.', 'bonestheme' ); ?></p>
-						</section>
-						<footer class="article-footer">
-								<p><?php _e( 'This is the error message in the page-custom.php template.', 'bonestheme' ); ?></p>
-						</footer>
-					</article>
-
-			<?php endif; ?>
 
 
 	</main><!-- /#content -->
